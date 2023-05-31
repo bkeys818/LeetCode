090e6ff86d47f128e57ae5c6b5a0e1386e4ec9ec
Ben Keys <bkeys818@gmail.com>
2023-05-31 11:50:12 -0400

    🧪 Update tests

--- tests/new-21-game.test.ts
+++ tests/new-21-game.test.ts
@@ -1,11 +1,11 @@
 import solution from '@/new-21-game'
 
 describe('New 21 Game', () => {
-    test.each([
-        [1, [10, 1, 10], 1],
-        [2, [6, 1, 10], 0.6],
-        [3, [21, 17, 10], 0.73278],
-    ])(`case %i`, (_, args: [number, number, number], expected) => {
+    test.each<[number, [number, number, number]]>([
+        [1, [10, 1, 10]],
+        [0.6, [6, 1, 10]],
+        [0.73278, [21, 17, 10]],
+    ])(`case %#`, (expected, args) => {
         expect(solution(...args)).toBeCloseTo(expected, 5)
     })
 })
