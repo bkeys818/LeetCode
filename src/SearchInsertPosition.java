fa5151a3c4a12ad1d4105c26e9e99dc09c5b1c89
Ben Keys <bkeys818@gmail.com>
2023-06-07 15:40:41 -0400

    📙 Add new problem
    Search Insert Position

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,13 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = (end + start) / 2;
+            if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid;
+        }
+        return nums[start] < target ? start + 1 : start;
+    }
+}
